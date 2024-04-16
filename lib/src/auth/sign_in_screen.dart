6a5a53c94e3f6afef2e4582fc0267d5c5f98e8c6
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2024-04-16 18:34:29 -0300

    criação do botão de entrar e do link para esqueceu a senha

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bluegrocer/src/auth/components/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
@@ -27,19 +28,48 @@ class SignInScreen extends StatelessWidget {
                     top: Radius.circular(45),
                   )),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   // email
-                  CustomTextField(
+                  const CustomTextField(
                     icon: Icons.email,
                     label: 'Email',
                   ),
 
                   // senha
-                  CustomTextField(
+                  const CustomTextField(
                     icon: Icons.lock,
                     label: 'Senha',
                     isSecret: true,
                   ),
+
+                  // botão de entrar
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18))),
+                      onPressed: () {},
+                      child: const Text(
+                        'Entrar',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ),
+
+                  // esqueceu a senha
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Esqueceu a senha?',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                        )),
+                  )
                 ],
               ),
             ),
