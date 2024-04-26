6299458ba01797a1a92fd3baa543c2142a31a634
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2024-04-26 00:31:20 -0300

    Criação do showDialog de confirmação de pedido

--- lib/src/pages/cart/cart_tab.dart
+++ lib/src/pages/cart/cart_tab.dart
@@ -89,7 +89,11 @@ class _CartTabState extends State<CartTab> {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18),
                         )),
-                    onPressed: () {},
+                    onPressed: () async {
+                      bool? result = await showOrderConfirmation();
+
+                      print(result);
+                    },
                     child: const Text(
                       'Concluir Pedido',
                       style: TextStyle(
@@ -103,6 +107,39 @@ class _CartTabState extends State<CartTab> {
           )
         ],
       ),
+    );
+  }
+
+  Future<bool?> showOrderConfirmation() {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          title: const Text('Confirmação'),
+          content: const Text('Deseja realmente concluir o pedido?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Não'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Sim'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
